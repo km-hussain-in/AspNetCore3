8a8521904f81bb50fd3fca7b4f2f6af1fd189289
km-hussain-in <km.hussain.in@gmail.com>
2019-04-05 09:39:56 -0400

    Latest

--- WebApiDemo/DemoApp/Startup.cs
+++ WebApiDemo/DemoApp/Startup.cs
@@ -9,17 +9,25 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DemoApp
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         	//services.AddDbContext<Models.AppDbContext>(options => options.UseInMemoryDatabase("appdb"));
-        	services.AddDbContext<Models.AppDbContext>(options => options.UseSqlite("FileName=app.db"));
+        	services.AddDbContext<Models.AppDbContext>(options => options.UseSqlite(Configuration.GetConnectionString("AppDbConnection")));
             services.AddMvc().AddNewtonsoftJson();      
         }
 
