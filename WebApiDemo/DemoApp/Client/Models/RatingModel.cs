c4d67347054cc7fc586531262a94d34896940cab
km-hussain-in <km.hussain.in@gmail.com>
2019-04-15 12:15:29 -0400

    Latest

---
+++ WebApiDemo/DemoApp/Client/Models/RatingModel.cs
@@ -0,0 +1,38 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace DemoApp.Client.Models
+{
+    [DataContract]
+    public class Rating
+    {
+        [DataMember(Name = "from")]
+        public string Name {get; set;}
+
+        [DataMember(Name = "rating")]
+        public int Rank {get; set;}
+    }
+
+    public class RatingModel
+    {
+        private HttpClient _client;
+
+        public RatingModel(HttpClient client) => _client = client;
+
+        public async Task<Rating> ReadRatingAsync(string name)
+        {
+            var response = await _client.GetAsync($"rest/feedbacks/secondary/{name}");
+            if(response.IsSuccessStatusCode)
+                return await response.Content.ReadAsAsync<Rating>();
+            return null;
+        }
+
+        public async Task<int> WriteRatingAsync(Rating info)
+        {
+            var response = await _client.PostAsJsonAsync("rest/feedbacks/secondary", info);
+            return (int)response.StatusCode;
+        }
+    }
+}
