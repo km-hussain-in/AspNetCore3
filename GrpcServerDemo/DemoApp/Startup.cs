012e5283523ef231fcadac945b5942b1ce65abc8
km-hussain-in <km.hussain.in@gmail.com>
2019-10-27 13:16:16 +0530

    Latest

--- GrpcServerDemo/DemoApp/Startup.cs
+++ GrpcServerDemo/DemoApp/Startup.cs
@@ -32,12 +32,7 @@ namespace DemoApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<Services.ShopKeeperService>();
-
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-                });
-            });
+           });
         }
     }
 }
